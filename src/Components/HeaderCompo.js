b722d4a1ae1856eafc653b84e39a2465eea9904b
pranav <pranavbavaskar1@gmail.com>
2023-08-24 14:44:02 +0530

    s

--- src/Components/HeaderCompo.js
+++ src/Components/HeaderCompo.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 // import NavBar from "./NavBar/NavBar";
 import Logo from "./Logo/Logo";
 import "./Logo/logo.css";
-import { useEffect } from "react";
+
 import NewNav from "./NewNav/NewNav";
 
 const HeaderCompo = () => {
